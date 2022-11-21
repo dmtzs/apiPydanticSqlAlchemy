b9573fa5c04277bf67fef425efb41a1b493a0010
Diego Martínez Sánchez <diego.mtzs00@gmail.com>
2022-11-21 16:40:21 -0600

    Final update
    Justo a comment in run_app.py file

--- run_app.py
+++ run_app.py
@@ -1,6 +1,6 @@
 try:
     from app import app
-    from gevent.pywsgi import WSGIServer
+    # from gevent.pywsgi import WSGIServer
 except ImportError as eImp:
     print(f"The following import ERROR occurred in {__file__}: {eImp}")
 
