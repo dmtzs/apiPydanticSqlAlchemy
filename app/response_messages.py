b9b59dd944947df8fad18f4b87a102ef706a958a
dmtzs <diegomartinez@tuhabi.mx>
2022-11-22 08:28:42 -0600

    Update response_message.py
    Added more fields to the responses inside this file

--- app/response_messages.py
+++ app/response_messages.py
@@ -1,2 +1,8 @@
-no_users = {"message": "No users found"}
-validation_data_error = {"error": "Validation error inside data array"}
+no_users = {
+    "type": "no_users",
+    "message": "No users found"
+}
+validation_data_error = {
+    "type": "validation_error",
+    "error": "Validation error inside data array"
+}
